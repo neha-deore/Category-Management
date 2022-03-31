3df3d59a4d99aeebc630e293fa53cd2b79a323f2
neha-deore <90671762+neha-deore@users.noreply.github.com>
2022-03-31 11:57:12 +0530

    Add files via upload

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 import path from "path";
 
-const DIR = "./uploads/";
+const DIR = "../frontend/public/images/";
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, DIR);
